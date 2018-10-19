247d317bc64dae971dfe49bb4304d536058af38f
KostyaPityk <konstantin.pityk@gmail.com>
2018-10-19 15:02:51 +0300

    Third task from the tasks

---
+++ NET1.A.2018.Pityk.04/GCD.Test/GCD.Test.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace GCD.Test
+{
+    [TestFixture]
+    public class GCDTest
+    {
+        #region Test from two elements
+        [TestCase(10, 100, ExpectedResult = 10)]
+        [TestCase(36, 90, ExpectedResult = 18)]
+        [TestCase(39, 130, ExpectedResult = 13)]
+        [TestCase(-5, 90, ExpectedResult = 5)]
+        [TestCase(100, 20, ExpectedResult = 20)]
+        public int EucideanTests_TwoElementsValidData_ValidResult(int first, int second)
+        {
+            return GCD.EuclidGCD(out _, first, second).GCD;
+        }
+
+        [TestCase(10, 100, ExpectedResult = 10)]
+        [TestCase(36, 90, ExpectedResult = 18)]
+        [TestCase(39, 130, ExpectedResult = 13)]
+        [TestCase(-5, 90, ExpectedResult = 5)]
+        [TestCase(0, 1, ExpectedResult = 1)]
+        [TestCase(0, 10000, ExpectedResult = 10000)]
+        public int SteinTests_TwoElementsValidData_ValidResult(int first, int second)
+        {
+            return GCD.SteinGDC(out _, first, second).GCD;
+        }
+
+        #endregion
+
+        #region Test from three elements
+        [TestCase(10, 100, -50, ExpectedResult = 10)]
+        [TestCase(36, 90, 216, ExpectedResult = 18)]
+        [TestCase(39, 130, 52, ExpectedResult = 13)]
+        [TestCase(-5, 90, 35, ExpectedResult = 5)]
+        [TestCase(100, 20, 60, ExpectedResult = 20)]
+        public int EucideanTests_ThreeElementsValidData_ValidResult(int first, int second, int third)
+        {
+            return GCD.EuclidGCD(out _, first, second, third).GCD;
+        }
+
+        [TestCase(10, 100, -50, ExpectedResult = 10)]
+        [TestCase(36, 90, 216, ExpectedResult = 18)]
+        [TestCase(39, 130, 52, ExpectedResult = 13)]
+        [TestCase(-5, 90, 35, ExpectedResult = 5)]
+        [TestCase(0, 1, 2, ExpectedResult = 1)]
+        [TestCase(0, 0, 10000, ExpectedResult = 10000)]
+        public int SteinTests_ThreeElementsValidData_ValidResult(int first, int second, int third)
+        {
+            return GCD.SteinGDC(out _, first, second, third).GCD;
+        }
+
+        #endregion
+
+        #region Test from four elements
+        [TestCase(10, 100, -50, 30, ExpectedResult = 10)]
+        [TestCase(36, 90, 216, 72, ExpectedResult = 18)]
+        [TestCase(39, 130, 52, 156, ExpectedResult = 13)]
+        [TestCase(-5, 90, 35, -95, ExpectedResult = 5)]
+        public int EucideanTests_FourElementsValidData_ValidResult(int first, int second, int third, int four)
+        {
+            return GCD.EuclidGCD(out _, first, second, third, four).GCD;
+        }
+
+        [TestCase(10, 100, -50, 30, ExpectedResult = 10)]
+        [TestCase(36, 90, 216, 72, ExpectedResult = 18)]
+        [TestCase(39, 130, 52, 156, ExpectedResult = 13)]
+        [TestCase(-5, 90, 35, -95, ExpectedResult = 5)]
+        public int SteinTests_FourElementsValidData_ValidResult(int first, int second, int third, int four)
+        {
+            return GCD.SteinGDC(out _, first, second, third, four).GCD;
+        }
+        #endregion
+
+        #region Test from five elements
+        [TestCase(10, 100, -50, 30, -70, ExpectedResult = 10)]
+        [TestCase(36, 90, 216, 72, 252, ExpectedResult = 18)]
+        public int EucideanTests_FiveElementsValidData_ValidResult(int first, int second, int third, int four, int five)
+        {
+            return GCD.EuclidGCD(out _, first, second, third, four, five).GCD;
+        }
+
+        [TestCase(10, 100, -50, 30, -70, ExpectedResult = 10)]
+        [TestCase(36, 90, 216, 72, 252, ExpectedResult = 18)]
+        public int SteinTests_FiveElementsValidData_ValidResult(int first, int second, int third, int four, int five)
+        {
+            return GCD.SteinGDC(out _, first, second, third, four, five).GCD;
+        }
+        #endregion
+
+        #region Exceptions
+        [TestCase]
+        public void SteinTests_LessThenThowElements_ArgumentException()
+           => Assert.Throws(typeof(ArgumentException), () => GCD.SteinGDC(out _, 1));
+
+        [TestCase]
+        public void SteinTests_EmptyArray_ArgumentNullException()
+           => Assert.Throws(typeof(ArgumentNullException), () => GCD.SteinGDC(out _, null));
+
+        [TestCase]
+        public void EucideanTests_LessThenThowElements_ArgumentException()
+           => Assert.Throws(typeof(ArgumentException), () => GCD.EuclidGCD(out _, 1));
+
+        [TestCase]
+        public void EucideanTests_EmptyArray_ArgumentNullException()
+           => Assert.Throws(typeof(ArgumentNullException), () => GCD.EuclidGCD(out _, null));
+        #endregion
+
+        #region Test Time
+        [Test]
+        public void EucideanTests_CheakTime()
+        {
+            int timeFromOut;
+            int timeFromTuple = GCD.EuclidGCD(out timeFromOut, 9, 33, 90).Time;
+            Assert.AreEqual(timeFromOut, timeFromTuple);
+        }
+
+        [Test]
+        public void SteinTests_TestsCheakTime()
+        {
+            int timeFromOut;
+            int timeFromTuple = GCD.SteinGDC(out timeFromOut, 9, 33, 90).Time;
+            Assert.AreEqual(timeFromOut, timeFromTuple);
+        }
+        #endregion
+    }
+}
---
+++ NET1.A.2018.Pityk.04/GCD/GCD.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace GCD
+{
+    /// <summary>
+    /// Represents algorithms for evaluating 
+    /// Greatest Common Divisor (GCD). Includes two methods for evaluating GDC
+    /// </summary>
+    public static class GCD
+    {
+        #region Public methods
+        /// <summary>
+        /// Represents method of evaluating GDC using Euclidean algorithm. Receive arbitrary arguments.
+        /// </summary>
+        /// <param name="time">Time of execution in milliseconds</param>
+        /// <param name="numbers">Numbers</param>
+        /// <returns>GDC for given data and time work</returns>
+        public static (int GCD, int Time) EuclidGCD(out int time, params int[] numbers)
+        {
+            CheckFromNull(numbers);
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+
+            int GDC = Euclidean(numbers);
+
+            timer.Stop();
+
+            time = timer.Elapsed.Milliseconds;
+            return (GDC, time);
+        }
+
+        /// <summary>
+        /// Represents method of evaluating GDC using Stein algorithm. Receive arbitrary arguments.
+        /// </summary>
+        /// <param name="time">Time of execution in milliseconds</param>
+        /// <param name="numbers">Numbers</param>
+        /// <returns>GDC for given data and time work</returns>
+        public static (int GCD, int Time) SteinGDC(out int time, params int[] numbers)
+        {
+            CheckFromNull(numbers);
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+
+            int GDC = Stein(numbers);
+
+            timer.Stop();
+
+            time = timer.Elapsed.Milliseconds;
+            return (GDC, time);
+        }
+
+        #endregion
+
+        #region Private methods
+        /// <summary>
+        /// Represents algorithm of Euclid for evaluation GDC 
+        /// </summary>
+        /// <param name="first">First number</param>
+        /// <param name="second">Second number</param>
+        /// <returns>GDC of given numbers</returns>
+        private static int Euclidean(int first, int second)
+        {
+            first = Math.Abs(first);
+            second = Math.Abs(second);
+
+            while (first != second)
+            {
+                if (first > second)
+                {
+                    first = first - second;
+                }
+                else
+                {
+                    second = second - first;
+                }
+            }
+
+            return first;
+        }
+
+        /// <summary>
+        /// Represents algorithm of Stein for evaluation GDC 
+        /// </summary>
+        /// <param name="first">First number</param>
+        /// <param name="second">Second number</param>
+        /// <returns>GDC of given numbers</returns>
+        private static int Stein(int first, int second)
+        {
+            first = Math.Abs(first);
+            second = Math.Abs(second);
+
+            if (first == second)
+            {
+                return first;
+            }
+               
+            if (first == 0)
+            {
+                return second;
+            }
+
+            if (second == 0)
+            {
+                return first;
+            }
+
+            if ((~first & 1) != 0)
+            {
+                if ((second & 1) != 0)
+                {
+                    return Stein(first >> 1, second);
+                }
+                else
+                {
+                    return Stein(first >> 1, second >> 1) << 1;
+                }
+            }
+
+            if ((~second & 1) != 0)
+            {
+                return Stein(first, second >> 1);
+            }
+
+            if (first > second)
+            {
+                return Stein((first - second) >> 1, second);
+            }
+
+            return Stein((second - first) >> 1, first);
+        }
+
+        /// <summary>
+        /// Represents algorithm of Euclidean for evaluation GDC 
+        /// </summary>
+        /// <param name="numbers">First number</param>
+        /// <returns>GDC of given numbers</returns>
+        private static int Euclidean(params int[] numbers)
+        {
+            int GDC = Euclidean(numbers[0], numbers[1]);
+
+            for (int i = 2; i < numbers.Length; i++)
+            {
+                GDC = Euclidean(GDC, numbers[i]);
+            }
+
+            return GDC;
+        }
+
+        /// <summary>
+        /// Represents algorithm of Stein for evaluation GDC 
+        /// </summary>
+        /// <param name="numbers">First number</param>
+        /// <returns>GDC of given numbers</returns>
+        private static int Stein(params int[] numbers)
+        {
+            int GDC = Stein(numbers[0], numbers[1]);
+
+            for (int i = 2; i < numbers.Length; i++)
+            {
+                GDC = Stein(GDC, numbers[i]);
+            }
+
+            return GDC;
+        }
+
+        /// <summary>
+        /// Check from Null
+        /// </summary>
+        /// <param name="numbers">Check array</param>
+        /// <exception cref="ArgumentNullException">
+        /// Raises if given arguments represents as empty array
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        /// Raises if given arguments represents as array with length less 2
+        /// </exception>
+        private static void CheckFromNull(int[] numbers)
+        {
+            if (numbers == null)
+            {
+                throw new ArgumentNullException(nameof(numbers), "There is no data to evaluate");
+            }
+
+            if (numbers.Length < 2)
+            {
+                throw new ArgumentException(nameof(numbers), "Check your data(can't be less than two numbers)");
+            }
+        }
+        #endregion
+    }
+}
