e929f0488e11ee85d3c43669b292a550b898cf89
KostyaPityk <konstantin.pityk@gmail.com>
2018-10-23 23:17:19 +0300

    complete Polimome task

---
+++ NET1.A.2018.Pityk.05/Polynomial.Test/Polynomial.Test.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using Polynomial;
+using NUnit.Framework;
+
+namespace Polynomial.Test
+{
+    [TestFixture]
+    public class PolynomialTest
+    {
+        #region Exceptions
+        [TestCase(null)]
+        public void PolynimialTest_CheckNullData(double[] coefficent)
+        => Assert.Throws(typeof(ArgumentNullException), () => new  Polynomial(coefficent));
+
+        [TestCase(new double[0])]
+        public void PolynimialTest_CheckNullLength(double[] coefficent)
+        => Assert.Throws(typeof(ArgumentException), () => new Polynomial(coefficent));
+        #endregion
+
+        #region Same Power of Polynomial
+        [TestCase(new double[] { 2, 3, 4 }, new double[] { 2, 2, 4 }, new double[] { 4, 5, 8 })]
+        [TestCase(new double[] { -2, -3, 4 }, new double[] { 2, 2, 4 }, new double[] { 0, -1, 8 })]
+        public void PolynomialTest_SamePower_PlusTests(double[] coefficent1, double[] coefficent2, double[] expected)
+        {
+            Polynomial polynomialFirst = new Polynomial(coefficent1);
+            Polynomial polinimialSecond = new Polynomial(coefficent2);
+            Polynomial polynomialResult = polynomialFirst + polinimialSecond;
+
+            Assert.AreEqual(new Polynomial(expected), polynomialResult);
+        }
+
+        [TestCase(new double[] { 2, 3, 4 }, new double[] { 2, 2, 4 }, new double[] { 0, 1, 0 })]
+        [TestCase(new double[] { -2, -3, 4 }, new double[] { 2, 2, 4 }, new double[] { -4, -5, 0 })]
+        public void PolynimialTest_SamePower_MinusTests(double[] coefficent1, double[] coefficent2, double[] expected)
+        {
+            Polynomial polynomialFirst = new Polynomial(coefficent1);
+            Polynomial polinimialSecond = new Polynomial(coefficent2);
+            Polynomial polynomialResult = polynomialFirst - polinimialSecond;
+
+            Assert.AreEqual(new Polynomial(expected), polynomialResult);
+        }
+
+        [TestCase(new double[] { 2, 3, 4 }, new double[] { 2, 2, 4 }, new double[] { 4, 10, 22, 20, 16 })]
+        [TestCase(new double[] { -2, -3, 4 }, new double[] { 2, 2, 4 }, new double[] { -4, -10, -6, -4, 16 })]
+        public void PolynomialTest_SamePower_MultiplicationTests(double[] coefficent1, double[] coefficent2, double[] expected)
+        {
+            Polynomial polynomialFirst = new Polynomial(coefficent1);
+            Polynomial polinimialSecond = new Polynomial(coefficent2);
+            Polynomial polynomialResult = polynomialFirst * polinimialSecond;
+
+            Assert.AreEqual(new Polynomial(expected), polynomialResult);
+        }
+        #endregion
+
+        #region Different Power Polynomial
+        [TestCase(new double[] { 4, 2, 3, 4 }, new double[] { 2, 2, 4 }, new double[] { 4, 0, 1, 0 })]
+        [TestCase(new double[] { -2, -3, 4 }, new double[] { 2, 2, 4 }, new double[] { -4, -5, 0 })]
+        [TestCase(new double[] { -8, 3, -2, -3, 4 }, new double[] { 2, 2, 4 }, new double[] { -8, 3, -4, -5, 0 })]
+        [TestCase(new double[] { -2, -3, 4, 7 }, new double[] { 8, 2, 2, 4, 7 }, new double[] { -8, -4, -5, 0, 0 })]
+        public void PolynimialTest_DifferentPower_MinusTests(double[] coefficent1, double[] coefficent2, double[] expected)
+        {
+            Polynomial polynomialFirst = new Polynomial(coefficent1);
+            Polynomial polinimialSecond = new Polynomial(coefficent2);
+            Polynomial polynomialResult = polynomialFirst - polinimialSecond;
+
+            Assert.AreEqual(new Polynomial(expected), polynomialResult);
+        }
+
+        [TestCase(new double[] { 4, 2, 3, 4 }, new double[] { 2, 2, 4 }, new double[] { 4, 4, 5, 8 })]
+        [TestCase(new double[] { -2, -3, 4 }, new double[] { 2, 2, 4 }, new double[] { 0, -1, 8 })]
+        [TestCase(new double[] { -8, 3, -2, -3, 4 }, new double[] { 2, 2, 4 }, new double[] { -8, 3, 0, -1, 8 })]
+        [TestCase(new double[] { -2, -3, 4, 7 }, new double[] { 8, 2, 2, 4, 7 }, new double[] { 8, 0, -1, 8, 14 })]
+        public void PolynimialTest_DifferentPower_PlusTests(double[] coefficent1, double[] coefficent2, double[] expected)
+        {
+            Polynomial polynomialFirst = new Polynomial(coefficent1);
+            Polynomial polinimialSecond = new Polynomial(coefficent2);
+            Polynomial polynomialResult = polynomialFirst + polinimialSecond;
+
+            Assert.AreEqual(new Polynomial(expected), polynomialResult);
+        }
+
+        [TestCase(new double[] { 4, 2, 3, 4 }, new double[] { 2, 2, 4 }, new double[] { 8, 12, 26, 22, 20, 16 })]
+        [TestCase(new double[] { -2, -3, 4 }, new double[] { 2, 2, 4 }, new double[] { -4, -10, -6, -4, 16 })]
+        [TestCase(new double[] { -8, 3, -2, -3, 4 }, new double[] { 2, 2, 4 }, new double[] { -16, -10, -30, 2, -6, -4, 16 })]
+        [TestCase(new double[] { -2, -3, 4, 7 }, new double[] { 8, 2, 2, 4, 7 }, new double[] { -16, -28, 22, 50, -4, 9, 56, 49 })]
+        public void PolynimialTest_DifferentPower_MultiplicationTests(double[] coefficent1, double[] coefficent2, double[] expected)
+        {
+            Polynomial polynomialFirst = new Polynomial(coefficent1);
+            Polynomial polinimialSecond = new Polynomial(coefficent2);
+            Polynomial polynomialResult = polynomialFirst * polinimialSecond;
+
+            Assert.AreEqual(new Polynomial(expected), polynomialResult);
+        }
+        #endregion
+
+        #region Override methods
+        [TestCase(new double[] { 5, 4 })]
+        [TestCase(new double[] { 7, 1, 5 })]
+        public void PolynomialTest_OverridingOperatorEqual(double[] data)
+        {
+            Polynomial first = new Polynomial(data);
+            Polynomial second = new Polynomial(data);
+            Polynomial third = new Polynomial(new double[] { 0, 0 });
+
+            Assert.True(first == second);
+            Assert.True(first.Equals(second));
+            Assert.True(first != third);
+        }
+
+        [TestCase(new double[] { 3, 10, 7 }, ExpectedResult = "3*x^2+10*x^1+7")]
+        [TestCase(new double[] { 3, 5, 7 }, ExpectedResult = "3*x^2+5*x^1+7")]
+        [TestCase(new double[] { -1, 1, 9 }, ExpectedResult = "-1*x^2+1*x^1+9")]
+        public string PolynimialTest_ToString(double[] coefficent)
+        {
+            Polynomial polynomial = new Polynomial(coefficent);
+            return polynomial.ToString();
+        }
+
+        [TestCase(ExpectedResult = true)]
+        public bool PolynomialTest_CheckOverrideGetHashCode()
+        {
+            Polynomial elementFirst = new Polynomial(new double[] { 7, 5, 9 });
+            Polynomial elementSecond = new Polynomial(new double[] { 7, 5, 9 });
+            return elementFirst.GetHashCode() == elementFirst.GetHashCode();
+        }
+        #endregion
+    }
+}
---
+++ NET1.A.2018.Pityk.05/Polynomial/Polynomial.cs
@@ -0,0 +1,313 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polynomial
+{
+    public sealed class Polynomial
+    {
+        private static readonly double COMPARISON = 10e-5;
+        private readonly double[] coefficients;
+
+        /// <summary>
+        /// Constructor of class
+        /// </summary>
+        /// <param name="polynomial_coefficients">Polynomial coefficients</param>
+        /// <exception cref="ArgumentNullException">
+        /// If Parameters should not be represent as null array
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        /// If Length should be great then zero
+        /// </exception>
+        public Polynomial(params double[] polynomial_coefficients)
+        {
+            CheckData(polynomial_coefficients);
+            coefficients = new double[polynomial_coefficients.Length];
+            polynomial_coefficients.CopyTo(coefficients, 0);
+        }
+
+        /// <summary>
+        /// Property to get power of Polynomial
+        /// </summary>
+        public int Power { get => coefficients.Length - 1; }
+
+        public double this[int index]
+        {
+            get
+            {
+                return coefficients[index];
+            }
+
+            private set
+            {
+                coefficients[index] = value;
+            }
+        }
+        #region Overriding methods
+        /// <summary>
+        /// Get string representation of polynomial
+        /// </summary>
+        /// <returns>String representation of polynomial</returns>
+        public override string ToString()
+        {
+            string stringPolynomial = string.Empty;
+            int power = GetCoefficients().Length - 1;
+            foreach (double coeff in GetCoefficients())
+            {
+                if (power == 0)
+                {
+                    stringPolynomial = string.Concat(stringPolynomial, $"{coeff}");
+                }
+                else
+                {
+                    stringPolynomial = string.Concat(stringPolynomial, $"{coeff}*x^{power}+");
+                }
+
+                power--;
+            }
+
+            return stringPolynomial;
+        }
+
+        /// <summary>
+        /// Equals of two polynomial objects
+        /// </summary>
+        /// <param name="another">Object to compare with</param>
+        /// <returns> Result of Equals </returns>
+        public override bool Equals(object another)
+        {
+            if (another == null || GetType() != another.GetType())
+            {
+                return false;
+            }
+            else
+            {
+                return this == (Polynomial)another;
+            }
+        }
+
+        /// <summary>
+        /// Get hash code of polynomial
+        /// </summary>
+        /// <returns>Hash code</returns>
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int hash = 17;
+                for (int i = 0; i < Power; i++)
+                {
+                    hash = hash * 23 + coefficients[i].GetHashCode();
+                }
+
+                return hash;
+            }
+        }
+
+        #endregion
+
+        #region Overriding operators
+        /// <summary>
+        /// Multiplication operator
+        /// </summary>
+        /// <param name="thirst">First operand</param>
+        /// <param name="second">Second operand</param>
+        /// <returns>Result of multiplication</returns>
+        public static Polynomial operator *(Polynomial first, Polynomial second)
+        {
+            double[] shortest = first.GetCoefficients();
+            double[] longest = second.GetCoefficients();
+
+            double[] result = new double[shortest.Length + longest.Length - 1];
+
+            for (int i = 0; i < shortest.Length; i++)
+            {
+                for (int j = 0; j < longest.Length; j++)
+                {
+                    result[i + j] += shortest[i] * longest[j];
+                }
+            }
+
+            return new Polynomial(result);
+        }
+
+        /// <summary>
+        /// Sum operator
+        /// </summary>
+        /// <param name="first">First operand</param>
+        /// <param name="second">Second operand</param>
+        /// <returns>Result of Sum</returns>
+        public static Polynomial operator +(Polynomial first, Polynomial second)
+        {
+            (double[] shortest, double[] longest) normalizedArrays = BringToSameState(first, second);
+            double[] shortest = normalizedArrays.shortest;
+            double[] longest = normalizedArrays.longest;
+
+            double[] result = new double[longest.Length];
+
+            for (int i = 0; i < longest.Length; i++)
+            {
+                result[i] = shortest[i] + longest[i];
+            }
+
+            return new Polynomial(result);
+        }
+
+        /// <summary>
+        /// Difference operator
+        /// </summary>
+        /// <param name="first">First operand</param>
+        /// <param name="second">Second First operand</param>
+        /// <returns>Result of difference</returns>
+        public static Polynomial operator -(Polynomial first, Polynomial second)
+        {
+            bool firstIsShortest = first.GetCoefficients().Length < second.GetCoefficients().Length;
+            (double[] shortest, double[] longest) normalizedArrays = BringToSameState(first, second);
+
+            double[] shortest = normalizedArrays.shortest;
+            double[] longest = normalizedArrays.longest;
+
+            double[] result = new double[longest.Length];
+
+            if (firstIsShortest)
+            {
+                for (int i = 0; i < longest.Length; i++)
+                {
+                    result[i] = shortest[i] - longest[i];
+                }
+            }
+            else
+            {
+                for (int i = 0; i < longest.Length; i++)
+                {
+                    result[i] = longest[i] - shortest[i];
+                }
+            }
+
+            return new Polynomial(result);
+        }
+
+        /// <summary>
+        /// Difference operator
+        /// </summary>
+        /// <param name="first">First operand</param>
+        /// <param name="second">Second First operand</param>
+        /// <returns>Result of difference</returns>
+        public static Polynomial operator -(double first, Polynomial second)
+        {
+            double[] singleValue = { first };
+            Polynomial result = new Polynomial(singleValue);
+            return result - second;
+        }
+
+        /// <summary>
+        /// Multiplication operator for single value and polynomial
+        /// </summary>
+        /// <param name="lhs">First operand</param>
+        /// <param name="rhs">Second operand</param>
+        /// <returns>Result of multiplication</returns>
+        public static Polynomial operator *(double first, Polynomial second)
+        {
+            double[] singleValue = { first };
+            Polynomial result = new Polynomial(singleValue);
+            return result * second;
+        }
+
+        /// <summary>
+        /// Equals operator
+        /// </summary>
+        /// <param name="first">First operand</param>
+        /// <param name="second">Second operand</param>
+        /// <returns>Result of Equals</returns>
+        public static bool operator ==(Polynomial first, Polynomial second)
+        {
+            if (ReferenceEquals(first, second))
+            {
+                return true;
+            }
+
+            if (ReferenceEquals(first, null) || ReferenceEquals(second, null))
+            {
+                return false;
+            }
+
+            if (first.Power != first.Power)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < first.coefficients.Length; i++)
+            {
+                if (!(Math.Abs(first[i] - second[i]) < COMPARISON))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Not Equals operator
+        /// </summary>
+        /// <param name="first">First operator</param>
+        /// <param name="second">Second operator</param>
+        /// <returns>Result of not Equals</returns>
+        public static bool operator !=(Polynomial first, Polynomial second)
+            => !(first == second);
+
+        private static (double[], double[]) BringToSameState(Polynomial leftCoefficents, Polynomial rightsCoefficents)
+        {
+            double[] first = leftCoefficents.GetCoefficients();
+            double[] second = rightsCoefficents.GetCoefficients();
+
+            double[] longest = (first.Length >= second.Length) ? first : second;
+            double[] shortest = (longest == first) ? second : first;
+
+            double[] result = new double[longest.Length];
+            Array.Copy(shortest, result, shortest.Length);
+
+            RightShift(ref result, longest.Length - shortest.Length);
+
+            return (result, longest);
+        }
+
+        private static void RightShift(ref double[] givenArray, int count)
+        {
+            count = count % givenArray.Length;
+            double[] tmp = new double[givenArray.Length];
+
+            for (int i = givenArray.Length - 1; i >= 0; i--)
+            {
+                tmp[i] = givenArray[(i - count + givenArray.Length) % givenArray.Length];
+            }
+
+            givenArray = tmp;
+        }
+
+        #endregion
+        #region Private methods
+        private void CheckData(double[] data)
+        {
+            if (data == null)
+            {
+                throw new ArgumentNullException(nameof(data), "Parameters should not be represent as null array");
+            }
+
+            if (data.Length == 0)
+            {
+                throw new ArgumentException(nameof(data), "Length should be great then zero");
+            }
+        }
+
+        private double[] GetCoefficients()
+        {
+            var copy = new double[coefficients.Length];
+            Array.Copy(coefficients, copy, coefficients.Length);
+            return copy;
+        }
+        #endregion
+    }
+}
