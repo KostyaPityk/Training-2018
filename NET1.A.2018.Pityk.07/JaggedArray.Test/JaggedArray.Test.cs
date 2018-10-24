75956e48a5838f5e52f7b7e6bc1de992a1ed13e4
KostyaPityk <konstantin.pityk@gmail.com>
2018-10-24 21:08:42 +0300

    the task of the seventh day

---
+++ NET1.A.2018.Pityk.07/JaggedArray.Test/JaggedArray.Test.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+
+namespace JaggedArray.Test
+{
+    [TestFixture]
+    public class JaggedArrayTest
+    {
+        [TestCase(null)]
+        public void PolynimialTest_CheckNullData(int[][] coefficent)
+        => Assert.Throws(typeof(ArgumentNullException), () => coefficent.Sort(new CompareBySumElement()));
+
+        [Test]
+        public void TestSort_MaxElementComparer_Random100ValidValue_ValueResult()
+        {
+            Random random_value = new Random(0);
+            const int SIZE = 100;
+            for (int i = 0; i < SIZE; i++)
+            {
+                int[][] array = CreateRandomArray(SIZE, random_value);
+
+                array.Sort(new CompareByMaxElement());
+                if (!IsSortedByKey(array, Enumerable.Max))
+                {
+                    Assert.Fail($"Test #{i} fail.");
+                }
+            }
+        }
+
+        [Test]
+        public void TestSort_MinElementComparer_Random100ValidValue_ValueResult()
+        {
+            Random random_value = new Random(0);
+            const int SIZE = 100;
+            for (int i = 0; i < SIZE; i++)
+            {
+                int[][] array = CreateRandomArray(SIZE, random_value);
+
+                array.Sort(new CompareByMinElement());
+                if (!IsSortedByKey(array, Enumerable.Min))
+                {
+                    Assert.Fail($"Test #{i} fail.");
+                }
+            }
+        }
+
+        [Test]
+        public void TestSort_SumElementComparer_Random100ValidValue_ValueResult()
+        {
+            Random random_value = new Random(0);
+            const int SIZE = 100;
+            for (int i = 0; i < SIZE; i++)
+            {
+                int[][] array = CreateRandomArray(SIZE, random_value);
+
+                array.Sort(new CompareBySumElement());
+                if (!IsSortedByKey(array, Enumerable.Sum))
+                {
+                    Assert.Fail($"Test #{i} fail.");
+                }
+            }
+        }
+        #region Private methods
+        private int[][] CreateRandomArray(int size, Random rng)
+        {
+            var array = new int[size][];
+
+            for (int j = 0; j < size; j++)
+            {
+                array[j] = new int[rng.Next(1, size)];
+            }
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < array[i].Length; j++)
+                {
+                    array[i][j] = rng.Next(-size, size);
+                }
+            }
+
+            return array;
+        }
+        private static bool IsSortedByKey(int[][] array, Func<int[], int> key)
+        {
+            int[] correct_result = array.Select(key).ToArray();
+            for (int i = 0; i < correct_result.Length - 1; i++)
+            {
+                if (correct_result[i] < correct_result[i + 1])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+        #endregion
+    }
+}
---
+++ NET1.A.2018.Pityk.07/JaggedArray/JaggedArray.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JaggedArray
+{
+    public static class JaggedArray
+    {
+        public static void Sort(this int[][] array, IComparer<int[]> compared)
+        {
+            CheckData(array);
+
+            array.BubbleSort(compared.Compare);
+        }
+
+        #region Private methods
+        private static void CheckData(int[][] data)
+        {
+            if (data == null)
+            {
+                throw new ArgumentNullException(nameof(data), "Parameters should not be represent as null array");
+            }
+        }
+
+        private static void BubbleSort(this int[][] array, Comparison<int[]> compared)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length - i - 1; j++)
+                {
+                    if (compared(array[j], array[j + 1]) > 0)
+                    {
+                        Swap(ref array[j], ref array[j + 1]);
+                    }
+                }
+            }
+        }
+
+        private static void Swap(ref int[] first, ref int[] second)
+        {
+            int[] temp = first;
+            first = second;
+            second = temp;
+        }
+        #endregion
+    }
+}
