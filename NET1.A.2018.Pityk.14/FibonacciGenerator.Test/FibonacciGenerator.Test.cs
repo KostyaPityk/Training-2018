c3a7c32e489576c04d2b8d1e02f2a427432c3fb2
KostyaPityk <konstantin.pityk@gmail.com>
2018-11-09 15:23:47 +0300

    generator fibonacci

---
+++ NET1.A.2018.Pityk.14/FibonacciGenerator.Test/FibonacciGenerator.Test.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+
+namespace FibonacciGenerator.Test
+{
+    [TestFixture]
+    public class FibonacciGeneratorTest
+    {
+        [TestCase(40, new  long[] { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025, 121393, 196418, 317811, 514229, 832040, 1346269, 2178309, 3524578, 5702887, 9227465, 14930352, 24157817, 39088169, 63245986, 102334155 })]
+        [TestCase(1, new long[] { 0, 1 })]
+        [TestCase(20, new long[] { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765 })]
+        [TestCase(30, new long[] { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025, 121393, 196418, 317811, 514229, 832040 })]
+        [TestCase(15, new long[] { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610 })]
+        [TestCase(22, new long[] { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711 })]
+        [TestCase(5, new long[] { 0, 1, 1, 2, 3, 5 })]
+        [TestCase(9, new long[] { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34 })]
+        [TestCase(35, new long[] { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025, 121393, 196418, 317811, 514229, 832040, 1346269, 2178309, 3524578, 5702887, 9227465 })]
+        public void FidonacciGeneratorTest_ValidData_ValidResult(int number, long[] expected)
+        {
+            var result = FibonacciGenerator.Fibonacci(number).ToArray();
+            CollectionAssert.AreEqual(result, expected);
+        }
+
+        [TestCase(0)]
+        [TestCase(-33)]
+        public void FidonacciGeneratorTest_InvalidData_ResultException(int number)
+            => Assert.Throws(typeof(ArgumentException), () => FibonacciGenerator.Fibonacci(number));
+    }
+}
---
+++ NET1.A.2018.Pityk.14/FibonacciGenerator/FibonacciGenerator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciGenerator
+{
+    /// <summary>
+    /// Represent funktion to calculate fibonacci numbers
+    /// </summary>
+    public static class FibonacciGenerator
+    {
+        /// <summary>
+        /// Calculates the Fibonacci sequence
+        /// </summary>
+        /// <param name="number">Number</param>
+        /// <returns>IEnumerable of fibonacci numbers</returns>
+        /// <exception cref="ArgumentException">
+        /// Throw if numbers less or equls zero
+        /// </exception>
+        public static IEnumerable<int> Fibonacci(int number)
+        {
+            CheckData(number);
+
+            yield return 0;
+
+            int first_number = 0;
+            int second_number = 1;
+
+            for (int i = 0; i < number; i++)
+            {  
+               
+                ChangeNumbers(ref first_number, ref second_number);
+                yield return first_number;
+            }
+        }
+
+        private static void CheckData(int number)
+        {
+            if (number <= 0)
+            {
+                throw new ArgumentException(nameof(number), "can`t be less or equls zero");
+            }
+        }
+
+        private static void ChangeNumbers(ref int first, ref int second)
+        {
+            int temp = first;
+            first = second;
+            second += temp;
+        }
+    }
+}
