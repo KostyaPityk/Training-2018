6f4346ae13b549d1867d0e256db9a815348136f0
KostyaPityk <konstantin.pityk@gmail.com>
2018-11-11 23:47:52 +0300

    Task Filter

---
+++ NET1.A.2018.Pityk.14/Filter.Test/Filter.TEst.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using Filter;
+
+namespace FilterTest
+{
+    [TestFixture]
+    public class FilterTest
+    {
+        [TestCase(new string[] { "one", "two", "three", "four", "five" }, 3, new string[] { "one", "two" })]
+        [TestCase(new string[] { "one", "two", "three", "four", "five" }, 5, new string[] { "three" })]
+        [TestCase(new string[] { "one", "two", "three", "four", "five" }, 4, new string[] { "four", "five" })]
+        public void TestFilter_LengthString_ValidData_ValidResult(IEnumerable<string> data, int length, IEnumerable<string> expected)
+        => CollectionAssert.AreEqual(data.Filter(x => x.Length == length), expected);
+
+        [TestCase(new string[] { "one", "two", "three", "four", "five" }, "o", new string[] { "one", "two", "four" })]
+        [TestCase(new string[] { "one", "two", "three", "four", "five" }, "three", new string[] { "three" })]
+        public void TestFilter_ContainsString_ValidData_ValidResult(IEnumerable<string> data, string line, IEnumerable<string> expected)
+        => CollectionAssert.AreEqual(data.Filter(x => x.Contains(line)), expected);
+
+        [TestCase(new string[] { "one", "two", "three", "four", "five" }, ".", new string[] { "one.", "two.", "three.", "four.", "five." })]
+        [TestCase(new string[] { "one", "two", "three", "four", "five" }, "!", new string[] { "one!", "two!", "three!", "four!", "five!" })]
+        [TestCase(new string[] { "one", "two", "three", "four", "five" }, " elephant", new string[] { "one elephant", "two elephant", "three elephant", "four elephant", "five elephant" })]
+        public void TestTransformer_AddedString_ValidData_ValidResult(IEnumerable<string> data, string line, IEnumerable<string> expected)
+        => CollectionAssert.AreEqual(data.Transformer(x => x + line), expected);
+
+        [TestCase(new int[] { 1, 2, 3, 4, 5 }, 3, new int[] { 4, 5, 6, 7, 8 })]
+        [TestCase(new int[] { 1, 2, 3, 4, 5 }, 1, new int[] { 2, 3, 4, 5, 6 })]
+        [TestCase(new int[] { 1, 2, 3, 4, 5 }, 4, new int[] { 5, 6, 7, 8, 9 })]
+        public void TestTransformer_AddedInt_ValidData_ValidResult(IEnumerable<int> data, int numbers, IEnumerable<int> expected)
+        => CollectionAssert.AreEqual(data.Transformer(x => x + numbers), expected);
+
+        [TestCase(new int[] { 1, 2, 3, 4, 5 }, 3, new int[] {3 })]
+        [TestCase(new int[] { 1, 2, 3, 4, 5 }, 1, new int[] { 1, 2, 3, 4, 5 })]
+        [TestCase(new int[] { 1, 2, 3, 4, 44 }, 4, new int[] { 4, 44 })]
+        public void TestFilter_Int_TheRemainder_ValidData_ValidResult(IEnumerable<int> data, int remainder, IEnumerable<int> expected)
+        => CollectionAssert.AreEqual(data.Filter(x => x % remainder == 0), expected);
+
+        [Test]
+        public void Filter_NullData_ArgumentNullException()
+        {
+            IEnumerable<string> data = null;
+            Assert.Throws(typeof(ArgumentNullException), () => data.Filter(x => x.Length == 3));
+        }
+
+        [Test]
+        public void Transformer_NullData_ArgumentNullException()
+        {
+            IEnumerable<string> data = null;
+            Assert.Throws(typeof(ArgumentNullException), () => data.Transformer(x => x + "test"));
+        }
+    }
+}
---
+++ NET1.A.2018.Pityk.14/Filter/CollectionFilter.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Filter
+{
+    /// <summary>
+    /// Represents generic methods Filter and Transformer
+    /// </summary>
+    public static class CollectionFilter
+    {
+        /// <summary>
+        /// Filter source data for a given predicate
+        /// </summary>
+        /// <typeparam name="T">Type</typeparam>
+        /// <param name="source">Source IEnumerable type</param>
+        /// <param name="predicate">Delegate </param>
+        /// <returns>Filter source for a given predicate</returns>
+        public static IEnumerable<T> Filter<T>(this IEnumerable<T> source, Func<T, bool> predicate)
+         {
+            if (source == null)
+            {
+                throw new ArgumentNullException($"{nameof(source)} can't be equal to null!");
+            }
+
+            if (predicate == null)
+            {
+                throw new ArgumentNullException($"{nameof(predicate)} can't be equal to null!");
+            }
+
+            return source.Filtration(predicate);
+        }
+
+        /// <summary>
+        /// Transformer source data for a given predicate
+        /// </summary>
+        /// <typeparam name="TSource">Type source</typeparam>
+        /// <typeparam name="TResult">Type result</typeparam>
+        /// <param name="source">Source IEnumerable type</param>
+        /// <param name="predicate">Delegate</param>
+        /// <returns>Transform source for a given predicate</returns>
+        public static IEnumerable<TResult> Transformer<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, TResult> predicate)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException($"{nameof(source)} can't be equal to null!");
+            }
+
+            if (predicate == null)
+            {
+                throw new ArgumentNullException($"{nameof(predicate)} can't be equal to null!");
+            }
+
+            return source.Transformation(predicate);
+        }
+
+        #region Private methods
+        private static IEnumerable<T> Filtration<T>(this IEnumerable<T> source, Func<T, bool> predicate)
+        {
+            foreach (T item in source)
+            {
+                if (predicate(item))
+                {
+                    yield return item;
+                }
+            }
+        }
+
+        private static IEnumerable<TResult> Transformation<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, TResult> predicate)
+        {
+            foreach (TSource item in source)
+            {
+                yield return predicate(item);
+            }
+        }
+        #endregion
+    }
+}
